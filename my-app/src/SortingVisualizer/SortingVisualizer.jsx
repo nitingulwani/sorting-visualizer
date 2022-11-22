e4d86356a29ecf2b965636d2b9fb66ec25c4398e
Nitin <gulwanin35@gmail.com>
2022-11-22 23:01:36 +0530

    added files

--- my-app/src/SortingVisualizer/SortingVisualizer.jsx
+++ my-app/src/SortingVisualizer/SortingVisualizer.jsx
@@ -22,16 +22,33 @@ export default class SortingVisualizer extends React.Component {
       this.setState({array});
     }
 
+    mergeSort() {}
+    
+    quickSort() {}
+
+    heapSort() {}
+
+    bubbleSort() {}
+
     render() {
         const {array} = this.state;
 
         return (
             <div className="array-container">
             {array.map((value, idx) => (
-                <div className="array-bar" key={idx}>
-                style={{height: `${value}px`}}
+                <div className="array-bar" 
+                key={idx}
+                style={{height: `${value}px`}}>
+                
                 </div>
             ))}
+
+            <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
+            <button onClick={() => this.quickSort()}>Quick Sort</button>
+            <button onClick={() => this.heapSort()}>Heap Sort</button>
+            <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+
             </div>
         );
     }
