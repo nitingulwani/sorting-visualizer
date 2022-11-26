af0aa1353293f2950156d64a8264f0c6ab603cad
Nitin <gulwanin35@gmail.com>
2022-11-26 12:12:56 +0530

    added files

--- my-app/src/SortingVisualizer/SortingVisualizer.jsx
+++ my-app/src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,16 +16,18 @@ export default class SortingVisualizer extends React.Component {
     }
   
     resetArray() {
-      const array = [];
-      for (let i = 0; i < 310; i++) {
+      for(let i = 0; i<310; i++){
         array.push(randomIntFromInterval(5, 730));
       }
       this.setState({array});
     }
 
     mergeSort() {
-      const javaScriptSortedArray = this.state.array.slice().sort();
+      const javaScriptSortedArray = this.state.array
+      .slice()
+      .sort((a, b) => a - b);  // we have to write this in javascript because for eg - 100 will come before 5 as 1 in 100 comes before 5
       const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+      // console.log(sortedArray);
 
       console.log(arraysareEqual(javaScriptSortedArray, sortedArray));
     }
@@ -35,6 +37,19 @@ export default class SortingVisualizer extends React.Component {
     heapSort() {}
 
     bubbleSort() {}
+
+    testSortingAlgorithms() {
+      for(let i = 0; i<100; i++){
+        const array = [];
+        const length = randomIntFromInterval(1, 1000);
+          for(let i = 0; i<length; i++){
+            array.push(randomIntFromInterval(-1000, 1000));
+          }
+          const javaScriptSortedArray = array.slice().sort((a, b) => a-b);
+          const mergeSortedArray = sortingAlgorithms.mergeSort(array.slice);
+          console.log(arraysareEqual(javaScriptSortedArray, mergeSortedArray));
+      }
+    }
 
     render() {
         const {array} = this.state;
@@ -54,21 +69,29 @@ export default class SortingVisualizer extends React.Component {
             <button onClick={() => this.quickSort()}>Quick Sort</button>
             <button onClick={() => this.heapSort()}>Heap Sort</button>
             <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            <button onClick={() => this.testSortingAlgorithms()}>Test Sorting Algorithms</button>
 
 
             </div>
         );
-    }
+    } 
 }  
+
+function generateNewArray() {
+      for(let i = 0; i<310; i++){
+        array.push(randomIntFromInterval(5, 730));
+      }
+}
 function randomIntFromInterval(min, max) {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
 function arraysareEqual(arrayOne, arrayTwo){
-  if(arrayOne.length !== arrayTwo) return false;
+  if(arrayOne.length !== arrayTwo.length) return false;
   for(let i = 0; i<arrayOne.length; i++){
     if(arrayOne[i] !== arrayTwo[i]) return false;
+    // console.log(arrayOne[i], arrayTwo[i]);
   }
   return true;
 }
